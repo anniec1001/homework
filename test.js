9781635a226ea83fb52b42df25bcc70874a85367
Cerrato <cerratoa@mia-cerratoa-m1.ds.ad.adp.com>
2016-06-13 21:59:52 -0700

    update

--- test.js
+++ test.js
@@ -1,3 +1,26 @@
 (function(window, document, undefined){
 	
+	var CTL = {
+		version: '0.0.1',
+	}
+
+	var total_elevators = [];  //no limit in total elevators
+
+	CTL.elevator = function(){
+		//initializing elevator properties
+		var elevator = {
+			id: id,
+			occupied: false,
+			trips: 0,
+			floor: 1,
+			destination:null,
+			floor_passed:0
+		}
+
+		total_elevators.push({'key':elevator.id, 'value':elevator});
+		return elevator;
+	}
+
+
+
 }(window, document));
