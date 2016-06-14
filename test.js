247a16ace2414b2605f5b4856e932ec3b87c0fe0
Cerrato <cerratoa@mia-cerratoa-m1.ds.ad.adp.com>
2016-06-13 22:13:04 -0700

    update

--- test.js
+++ test.js
@@ -5,11 +5,16 @@
 	}
 
 	var total_elevators = [];  //no limit in total elevators
+	var max_floor;
+
+	CTL.maxFloor = function(number){
+		max_floor = number;
+	}
 
 	CTL.elevator = function(){
 		//initializing elevator properties
 		var elevator = {
-			id: id,
+			id: generateId(), //assign an unique number for each elevator, assume generateId() will return a uniq id
 			occupied: false,
 			trips: 0,
 			floor: 1,
@@ -21,6 +26,19 @@
 		return elevator;
 	}
 
+	function generateId(){
+		return Math.floor(Math.random() * 2000);
+	}
 
+	var floors_inQueue = [];
+	CTL.floorRequested = function(onFloor){ //TODO: add floor requests in a queue, so we can complete all requests as new person make a request on a floor.
+		floors_inQueue.push(onFloor);
+		return findEv(onFloor);
+	}
+	
+	function findEv(floor){
+	}
+
+	window.CTL = CTL;
 
 }(window, document));
